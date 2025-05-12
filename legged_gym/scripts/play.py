2ce801ea41b04eb73d2ab087410a5b7083a8ce54
xihanM <2021307150305@cau.edu.cn>
2025-05-12 22:05:41 +0800

    添加debug代码

--- legged_gym/scripts/play.py
+++ legged_gym/scripts/play.py
@@ -1,3 +1,12 @@
+import debugpy
+try:
+    # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
+    debugpy.listen(("localhost", 9501))
+    print("Waiting for debugger attach")
+    debugpy.wait_for_client()
+except Exception as e:
+    pass
+
 import sys
 from legged_gym import LEGGED_GYM_ROOT_DIR
 import os
